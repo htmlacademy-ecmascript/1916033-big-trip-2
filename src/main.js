85aadf11c99ab7e94c8f030b5ecfb001c3a06764
h1ndy <155265302+h1ndy@users.noreply.github.com>
2024-11-13 22:36:02 +0300

    Шаблонизируй это

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import Presenter from './presenter/presenter.js';
+import PointModel from './model/way-point-model.js';
 
-const presenter = new Presenter();
+const pointModel = new PointModel;
+const presenter = new Presenter(pointModel);
 presenter.init();
